82cd8f9d88208227a899de68a8f87d0243513941
M <murumagnus@gmail.com>
2020-08-22 01:23:50 +0300

    AoP pointcut combos

--- basic-aop-pointcut-combo/src/main/java/com/example/aop/basic/aspect/LoggingAspect.java
+++ basic-aop-pointcut-combo/src/main/java/com/example/aop/basic/aspect/LoggingAspect.java
@@ -13,12 +13,26 @@ public class LoggingAspect {
     private void forDaoPackage() {
     }
 
-    @Before("forDaoPackage()")
+    // create pointcut for getters & setters
+    @Pointcut("execution(* com.example.aop.basic.*.*.get*(..))")
+    private void getter() {
+    }
+
+    @Pointcut("execution(* com.example.aop.basic.*.*.set*(..))")
+    private void setter() {
+    }
+
+    // combine getter and setter pointcuts
+    @Pointcut("forDaoPackage() && !(getter() || setter())")
+    private void forDaoPackageNoGetterSetter() {
+    }
+
+    @Before("forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n=====>>> Executing @Before advice on addAccount()");
     }
 
-    @Before("forDaoPackage()")
+    @Before("forDaoPackageNoGetterSetter()")
     public void preformAPIAnalytics() {
         System.out.println("\n=====>>> Preforming API Analytics");
     }
